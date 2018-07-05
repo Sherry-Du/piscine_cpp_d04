528e61d1377f13eeea29c7f9a3cb650ac7cc19aa
sherry-du <726845321@qq.com>
2018-07-05 15:13:39 +0800

    ex00

---
+++ ex00/droid.hh
@@ -0,0 +1,40 @@
+#ifndef _DROID_
+#define _DROID_
+
+#include <iostream>
+
+class Droid
+{
+    public:
+	Droid(std::string serial = "");
+	~Droid();
+	Droid(Droid const& droid);
+
+	std::string getId() const;
+	void setId(std::string serial);
+
+	size_t getEnergy() const;
+	void setEnergy(size_t energy);
+	
+	size_t getAttack() const;
+
+	size_t getToughness() const;
+
+	std::string* getStatus() const;
+	void setStatus(std::string* status);
+
+	bool operator==(Droid const& other) const;	
+	bool operator!=(Droid const& other) const;
+	Droid& operator=(Droid const& droid);
+	Droid& operator<<(size_t& other);
+    private:
+	std::string _serial;
+	size_t _energy;
+	size_t const _attack;
+	size_t const _toughness;
+	std::string* _status;
+};
+
+std::ostream& operator<<(std::ostream& os, Droid const& droid);
+
+#endif
